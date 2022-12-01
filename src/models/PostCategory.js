bbf40cfdac03f174886bcc2fe033f8adf6f9d82d
Bianca Oura <biancaoura@gmail.com>
2022-11-30 21:01:00 -0300

    feat: PostCategory model

---
+++ src/models/PostCategory.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    }
+  },
+  {
+    timestamps: false,
+    underscored: true,
+    tableName: 'posts_categories'
+  });
+
+  PostCategory.associate = ({ BlogPost, Category }) => {
+    BlogPost.belongsToMany(Category, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId'
+    });
+
+    Category.belongsToMany(BlogPost, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
